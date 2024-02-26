714556f44d847e33cbc28e9603ca58f1209f071d
marcmillie <mrmarcusmiller0@gmail.com>
2024-02-26 10:41:26 -0500

    colors and hex description added

---
+++ style_guide/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  return (
+  <div className="App">
+      <h1>My style Guide</h1>
+      <div className="container">
+        <h1>Colors</h1>
+        <p className="green">Light Green</p>
+        <p>#a1ef88</p>
+        <p className="gray">Dark Slate Gray</p>
+        <p>#0B5351</p>
+        <p className="periwinkle">Periwinkle</p>
+        <p>#B8CDF8</p>
+        <p className="fandango">Fandango</p>
+        <p>#BD2D87</p>
+        <p className="orchid">Orchid</p>
+        <p>#D664BE</p>
+      </div>
+      <div>
+        <div className="container">
+          <h1>Fonts</h1>
+          
+        </div>
+      </div>
+  </div>
+  );
+}
+
+export default App;
