a81415cb6353be3eeb15ae4be6b4c624a230defd
marcmillie <mrmarcusmiller0@gmail.com>
2024-02-27 00:00:04 -0500

    added all text to canvas

--- style_guide/src/App.js
+++ style_guide/src/App.js
@@ -20,7 +20,56 @@ function App() {
       <div>
         <div className="container">
           <h1>Fonts</h1>
-          
+          <div className='font-container'>
+            <div className='font-box'>
+              <p>Source Code Pro</p>
+              <p>Nunc sed augue lacus viverra vitae congue eu.</p>
+              <p>Nunc sed augue lacus viverra vitae congue eu.</p>
+              <p>Nunc sed augue lacus viverra vitae congue eu.</p>
+            </div>
+            <div className='font-box'>
+              <p>Source Code Pro</p>
+              <p>Nunc sed augue lacus viverra vitae congue eu.</p>
+              <p>Nunc sed augue lacus viverra vitae congue eu.</p>
+              <p>Nunc sed augue lacus viverra vitae congue eu.</p>
+            </div>
+            <div className='font-box'>
+              <p>Source Code Pro</p>
+              <p>Nunc sed augue lacus viverra vitae congue eu.</p>
+              <p>Nunc sed augue lacus viverra vitae congue eu.</p>
+              <p>Nunc sed augue lacus viverra vitae congue eu.</p>
+            </div>
+          </div>
+        </div>
+        <div className='container'>
+        <h1>Text Styles</h1>
+          <div className='text-container'>
+            <div className='text-box'>
+              <h1>H1: Main page heading</h1>
+              <ul>
+                <li>Font-weight:700 (bold)</li>
+                <li>Font-size: 26px</li>
+                <li>Font-family: Nunito Sans</li>
+              </ul>
+            </div>
+            <div className='text-box'>
+              <h1>H2: Subheading</h1>
+              <ul>
+                <li>Font-weight: 500</li>
+                <li>Font-size: 18px</li>
+                <li>Font-family: Poppins</li>
+              </ul>
+            </div>
+            <div className='text-box'>
+              <h1>P: Paragragh Text</h1>
+              <ul>
+                <li>Font-weight:400(regular)</li>
+                <li>Font-size: 14px</li>
+                <li>Font-family: Poppins</li>
+              </ul>
+            </div>
+          </div>
+
         </div>
       </div>
   </div>
